16ff163d13ee2198e2c7ba8afa976bb1cf08efb0
CigdemOzel <cigdemozel17@gmail.com>
2025-01-08 21:15:09 +0300

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Person person = new Person("John", "Doe", 25);
+        System.out.println(person.getFirstName());
     }
 }
